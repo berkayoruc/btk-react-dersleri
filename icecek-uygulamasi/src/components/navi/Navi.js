e4a189a307a19b840c1d8bd0f96418583e726363
Berkay <berkayoruc@hotmail.com>
2021-01-27 12:18:25 +0300

    yirmibirbitti

---
+++ icecek-uygulamasi/src/components/navi/Navi.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import {
+  Collapse,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+} from "reactstrap";
+
+class Navi extends Component {
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false,
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: this.state.isOpen,
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">LALA</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink>Form1 Demo</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink>Form2 Demo</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Cart
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    <NavLink>Go to cart</NavLink>
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default Navi;
